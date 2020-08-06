a4f4cd23fad4956763cef5bec75655fd2c6a85c5
emabytes <emadavor22@gmail.com>
2020-08-06 22:02:27 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import movies from "./data"
+import Movie from './Movie';
+console.log(movies)
+console.log(movies[0].genre[0])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        movies.map((movie, i) =>
+          <Movie
+            key={i}
+            title={movie.title}
+            year={movie.year}
+            director={movie.director}
+            duration={movie.duration}
+            rate={movie.rate}
+            genre={movie.genre}
+          />
+        )}
     </div>
   );
 }
---
+++ src/Movie.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <article id="movie">
+                <h3>{this.props.title}</h3>
+                <h4>{this.props.year}</h4>
+                <h4>{this.props.director}</h4>
+                <h4>{this.props.duration}</h4>
+                <h4>{this.props.rate}</h4>
+                <h5>{this.props.genre.map(ele => <div>{ele}</div>)}</h5>
+            </article>
+        );
+       
+    }
+}
+    
+export default Movie;
